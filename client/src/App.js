5fcab091efbf69c4916735e02b8ca9a5f888117e
Jitesh Vijay <jiteshvijay26@gmail.com>
2025-01-03 01:18:35 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Products from './Products';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Products />}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/Products/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from '../Header'
+
+const Products = () => {
+  return (
+    <Header />
+  )
+}
+
+export default Products
