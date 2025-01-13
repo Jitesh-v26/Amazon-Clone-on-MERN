33423cc99c14a0ffca7c1e12be9d945b1f97ceb2
Jitesh Vijay <jiteshvijay26@gmail.com>
2025-01-14 00:43:35 +0530

    rating logic complete

--- client/src/Product/index.js
+++ client/src/Product/index.js
@@ -8,26 +8,20 @@ import StarIcon from '@mui/icons-material/Star';
 const Products = () => {
 
   const [products, setProducts] = useState([]);
-  const [api, setApi] = useState(false);
+  // const [api, setApi] = useState(false);
 
   useEffect(()=>{
     fetch('https://fakestoreapi.com/products')
       .then(res=>res.json())
       .then(json=>{
-        setApi(true);
-        setProducts(json);
-        console.log("Product is here -->",products);
+        let temp = json;
+        temp = temp.map((item, index)=>{
+          return {...item, fullStar: Math.floor(item.rating.rate), halfStar: Math.floor(item.rating.rate) != item.rating.rate ? 1 : 0, borderStar: 5-Math.ceil(item.rating.rate)}
+        })
+      setProducts([...temp]);
+      console.log("Product in temp value -->",products);
       }); 
   }, [])
-
-  useEffect(()=>{
-    let temp = products;
-      temp = temp.map((item, index)=>{
-        return {...item, fullStar: Math.floor(item.rating.rate), halfStar: Math.floor(item.rating.rate) != item.rating.rate ? 1 : 0, borderStar: 5-Math.ceil(item.rating.rate)}
-      })
-      setProducts(temp);
-      console.log("Product in temp value -->",products);
-  }, [api]) 
 
   console.log(products);
 
@@ -42,6 +36,23 @@ const Products = () => {
               <img width="200px" height="200px" className='max-h-[200px]'  src={item.image} />
               <div className='w-[200px] text-wrap text-[#2162a1] cursor-pointer hover:underline'>{item.title}</div>
               <div className='text-[#b12704]'>${item.price}</div>
+              <div className='flex gap-[4px] justify-center'>
+                {[...Array(item.fullStar)].map((star, ind)=>{
+                  return(
+                    <StarIcon key={ind} sx={{color:'#de7921', width:'15px'}} />
+                  )
+                })}
+                {[...Array(item.halfStar)].map((star, ind)=>{
+                  return(
+                    <StarHalfIcon key={ind} sx={{color:'#de7921', width:'15px'}} />
+                  )
+                })}
+                {[...Array(item.borderStar)].map((star, ind)=>{
+                  return(
+                    <StarBorderIcon key={ind} sx={{color:'#de7921', width:'15px'}} />
+                  )
+                })}
+              </div>
             </div>
           )
         })}
